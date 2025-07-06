05bcd479c50302238dc1d9e2a73442a1cc34c8b5
Dacjan spezian Gapinski <me@dacjan.de>
2025-07-06 03:13:58 +0200

    Add health WIP

---
+++ components/health_component.gd
@@ -0,0 +1,31 @@
+class_name HealthComponent
+extends Node
+
+@export_subgroup("Settings")
+@export var maxHealth: float:
+	get:
+		return maxHealth
+	set(newMax):
+		maxHealth = newMax
+		if (currentHealth >= maxHealth):
+			currentHealth = maxHealth
+
+var hasHealthRemaining: bool:
+	get:
+		return is_equal_approx(currentHealth, 0)
+
+var currentHealth: float:
+	get:
+		return currentHealth
+	set(newHealth):
+		currentHealth = clamp(newHealth, 0, maxHealth)
+		if (!isDead && !hasHealthRemaining):
+			isDead = true
+
+var isDead: bool = false
+
+func initialise() -> void:
+	currentHealth = maxHealth
+
+func damage(amount: float) -> void:
+	currentHealth -= amount
--- scenes/player.gd
+++ scenes/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @export_subgroup("Nodes")
 @export var input_component: InputComponent
 @export var movement_component: MovementComponent
+@export var health_component: HealthComponent
+
+func _ready() -> void:
+	health_component.initialise()
 
 func _physics_process(delta: float) -> void:
 	movement_component.handle_movement(self, input_component.direction)
