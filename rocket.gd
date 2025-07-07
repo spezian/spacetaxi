ded66d53a764f56e89da3fbee718957a2ae7ea73
lev408 <157982525+lev408@users.noreply.github.com>
2025-07-07 18:11:23 +0200

    Verfolgungsrakete ist fertig.
    Sobald der Spieler die äußere Detection Area betritt, beginnt die Rakete ihn zu verfolgen. Wenn er die Area wieder verlässt, fliegt sie weiter. Man könnte die Verfolgungsrakete zu einem Projektil machen, das gegnerische Objekte schießen können.

--- rocket.gd
+++ rocket.gd
@@ -2,17 +2,43 @@ extends Node2D
 @export_subgroup("Settings")
 @export var SPEED: float
 @export var velocity_component: VelocityComponent
-@onready var rocket: Node2D = $"."
+var follow_player = false
+var character
 
 func _ready() -> void:
-	position = Vector2(320, 160)
+	position = Vector2(320, 120)
 
 func _physics_process(delta: float) -> void:
 	velocity_component.handle_movement(Vector2.LEFT)
+	if !follow_player:
+		position += velocity_component.velocity * delta
+	if follow_player:
+		position += (character.position - position)/ SPEED
+	print(position)
 	
-	position += velocity_component.velocity * delta
-
-
 func _on_detecting_area_body_entered(body: Node2D) -> void:
 	if body is Player:
-		pass
+		print(body.position)
+		print("entered")
+		character = body
+		follow_player = true
+		
+
+
+func _on_detecting_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		print(body.position)
+		print("exited")
+		character = null
+		follow_player = false
+		
+
+#func _on_detecting_area_area_entered(area: Area2D) -> void:
+	#if area is HurtboxComponent:
+		#follow_player = true
+		#character = area
+#
+#func _on_detecting_area_area_exited(area: Area2D) -> void:
+	#if area is HurtboxComponent:s
+		#follow_player = false
+		#character = null
