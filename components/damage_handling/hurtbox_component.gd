ec7818489dfe399c217719230f285acd86fc151a
lev408 <157982525+lev408@users.noreply.github.com>
2025-07-08 19:22:52 +0200

    Bug fix
    Hit- und Hurtboxen werden nun zwischen Gegnern und Playern differenziert, damit Gegner anderen Gegnern keinen Schaden hinzufügen können, sondern nur Playern.
    Außerdem können nun Hitboxen nach Bedarf nach einem Hit explodieren. Dies macht bspw. bei Verfolgungsraketen Sinn.

--- components/damage_handling/hurtbox_component.gd
+++ components/damage_handling/hurtbox_component.gd
@@ -3,9 +3,13 @@ extends Area2D
 
 @export_subgroup("Settings")
 @export var health_component: HealthComponent
+@export var is_enemy: bool = true
 
 func _on_area_entered(body: Node) -> void:
-	if (body is HitboxComponent):
+	if (body is HitboxComponent and !is_enemy):
 		var damage = body.get_damage()
 		health_component.damage(damage)
 		print("Damage: %d" % damage)
+	elif (body is HitboxComponent and !body.is_enemy):
+		var damage = body.get_damage()
+		health_component.damage(damage)
